53f8264d871ac88b867b0ba84f5e3483ca026a63
Mariya Hadzhiyska <136964468+DevMarieta@users.noreply.github.com>
2024-02-09 23:15:03 +0200

    .

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+// document.querySelector('.number').textContent = secretNumber;
+
+
+
+
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guesss = Number(document.querySelector('.guess').value);
+  console.log(guesss, typeof guesss);
+
+  if (!guesss) {
+    document.querySelector('.message').textContent = 'No Number!';
+  } else if (guesss === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.message').textContent = 'Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b464';
+  } else if (guesss < secretNumber) {
+    document.querySelector('.message').textContent = 'Toо Low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#70b534';
+  } else if (guesss > secretNumber) {
+    document.querySelector('.message').textContent = 'Toо Much!';
+    score--;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#60b774';
+  } else if (score < 0) {
+    document.querySelector('.message').textContent = 'You Lose The Game';
+  }
+});
+
+
+
+
+
+
+
+
+
 // console.log(document.querySelector('.message').textContent); //DOM manipulation
 // document.querySelector('.message').textContent = 'Corect Number!'; // декларираме промяната
 // console.log(document.querySelector('.message').textContent); //DOM manipulation
@@ -8,22 +52,32 @@
 
 // console.log(document.querySelector('.guess').value);
 // console.log(document.querySelector('.guess').value = 23);
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  //   if (!guesss) {
+  //     document.querySelector('.message').textContent =
+  //     'No Number!';
+  //   } else if(guesss === secretNumber){
+  //         document.querySelector('.message').textContent =
+  //         'Correct Number!';
+  //       } else if (guesss > secretNumber) {
+  //         if(score 0){
+
+  //         document.querySelector('.message').textContent =
+  //         'Too High!';
+  //         score--;
+  //         document.querySelector('.score').textContent = score;
+  //       } else if (guesss < secretNumber) {
+  //         document.querySelector('.message').textContent =
+  //         'Toо Low!';
+  //         score--;
+  //         document.querySelector('.score').textContent = score;
+  //       } else {
+  //         document.querySelector('.message').textContent =
+  //         'Game Over!';
+  //     }
+  //       }
 
 
-document.querySelector('.check').addEventListener
-('click', function () {
-  const guesss = Number(document.querySelector(
-    '.guess').value);
-  console.log(guesss, typeof guesss);
 
-  if (!guesss) {
-    document.querySelector('.message').textContent = 
-    'No Number!';
-  }else if(guesss === secretNumber) {
-    document.querySelector(".message").textContent = 
-    'Corect Number!';
-  }else if(guesss > secretNumber) {
-    document.querySelector(".message").textContent = 
-    'To Low!';
-});
+
+
