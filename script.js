1ac12bcc708a9d0e180eeb3803d20c0baa79c709
Mariya Hadzhiyska <136964468+DevMarieta@users.noreply.github.com>
2024-02-19 14:41:01 +0200

    .

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.oiverlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.open-modal');
+console.log(btnOpenModal);
+
+for( i= 0; i<btnOpenModal.length; btnOpenModal++){
+console.log(btnOpenModal[i].textContent);
+}
