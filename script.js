8cb5c16441286dd2357008e691082487ba395f83
Mariya Hadzhiyska <136964468+DevMarieta@users.noreply.github.com>
2024-02-20 23:31:04 +0200

    .

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 'use strict';
+
 const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.oiverlay');
+const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.open-modal');
 console.log(btnOpenModal);
 
-for( i= 0; i<btnOpenModal.length; btnOpenModal++){
-console.log(btnOpenModal[i].textContent);
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', function () {
+    console.log('Kura mi Qnko!');
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
 }
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
